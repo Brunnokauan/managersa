8774b66508e429d98d9ef5a6807a79aae8d21c54
Brunnokauan <brunno_kauan@hotmail.com>
2024-02-04 13:40:58 -0300

    Atualizando migrations

--- managersa/settings.py
+++ managersa/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'managersa.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://root:6dunAZu3Sq99pGdsDddd0sYmaXKnnnai@dpg-cliv461b2fgs73farrmg-a/manager_msa',
-        conn_max_age=600
-    )
+    # 'default': dj_database_url.config(
+    #     default='postgres://root:6dunAZu3Sq99pGdsDddd0sYmaXKnnnai@dpg-cliv461b2fgs73farrmg-a/manager_msa',
+    #     conn_max_age=600
+    # )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -111,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
